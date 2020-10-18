643e3f31fcea3df9827edcfd80a9f0c5cdcaeb14
Joel Robinson <44164513+JR325243@users.noreply.github.com>
2020-10-18 16:56:40 +0100

    Initial Commit

---
+++ Assets/InputManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class InputManager : MonoBehaviour
+{
+    [SerializeField] TMP_InputField forename, surname;
+    [SerializeField] TMP_Dropdown trait1, trait2, trait3;
+    [SerializeField] Slider connections, seperation;
+    [SerializeField] Toggle pos, neut, neg, rom;
+
+    public string protagForename, protagSurname;
+    public float pTrait1, pTrait2, pTrait3, conAm, degSep;
+    public bool posRel, neutRel, negRel, romRel;
+
+    public void UpdateName()
+    {
+        protagForename = forename.text;
+        protagSurname = surname.text;
+    }
+
+    public void UpdateTraits()
+    {
+        pTrait1 = trait1.value;
+        pTrait2 = trait2.value;
+        pTrait3 = trait3.value;
+    }
+
+    public void UpdateModifiers()
+    {
+        conAm = connections.value;
+        degSep = seperation.value;
+    }
+
+    public void UpdateRelationships()
+    {
+        posRel =  pos.isOn ? true : false;
+        neutRel = neut.isOn ? true : false;
+        negRel = neg.isOn ? true : false;
+        romRel = rom.isOn ? true : false;
+
+    }
+
+}
---
+++ Assets/UIManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    private void Start()
+    {
+        GameObject.Find("Start Screen").GetComponent<Transform>().localScale = Vector3.one;
+        GameObject.Find("Protag Screen").GetComponent<Transform>().localScale = Vector3.zero;
+        GameObject.Find("Main Screen").GetComponent<Transform>().localScale = Vector3.zero;
+    }
+
+    public void GetStarted()
+    {
+        StartCoroutine("GetStart");
+    }
+
+    public void Next()
+    {
+        StartCoroutine("Next1");
+    }
+
+    public IEnumerator GetStart()
+    {
+        LeanTween.scale(GameObject.Find("Start Screen"), Vector2.zero, 0.5f);
+        yield return new WaitForSeconds(0.5f);
+        LeanTween.scale(GameObject.Find("Protag Screen"), Vector2.one, 0.5f);
+    }
+
+    public IEnumerator Next1()
+    {
+        LeanTween.scale(GameObject.Find("Protag Screen"), Vector2.zero, 0.5f);
+        yield return new WaitForSeconds(0.5f);
+        LeanTween.scale(GameObject.Find("Main Screen"), Vector2.one, 0.5f);
+    }
+}
